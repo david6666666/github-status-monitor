1fb1235d517c9da98b986c2b2f34f535f635e468
WeiQing Chen <40507679+david6666666@users.noreply.github.com>
2025-08-14 20:47:39 +0800

    Update generate_stats.py

--- generate_stats.py
+++ generate_stats.py
@@ -7,7 +7,7 @@ from github import Github
 
 # =================================CONFIG=================================
 # The GitHub usernames you want to track
-USERNAMES = ["david6666666", "hsliuustc0106", "fake0fan", "Gongzq5", "zhouyeju", "knlnguyen1802", "R2-Y", "natureofnature", "ahengljh", "syedmba", "wuhang2014"]
+USERNAMES = ["david6666666", "hsliuustc0106", "fake0fan", "Gongzq5", "zhouyeju", "knlnguyen1802", "R2-Y", "natureofnature", "ahengljh", "syedmba", "wuhang2014", "chickeyton"]
 # Your GitHub Personal Access Token, read from an environment variable
 GITHUB_TOKEN = os.getenv('GH_PAT')
 # The output filename for the chart
